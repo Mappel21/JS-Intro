83712f291a1a73f1dcb3fc937117cb0713a3451b
Mappel21 <Mappel7676@gmail.com>
2021-06-21 17:06:47 +0000

    up to12

--- exercises/09-Calling-your-first-function/app.js
+++ exercises/09-Calling-your-first-function/app.js
@@ -4,3 +4,5 @@ function isOdd(myNumber)
 }
 
 // Your code below:
+let number=isOdd(45345)
+console.log(number)
--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,15 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+function testNum(a){
+if (a > 100){
+    return "We still have a bit of driving left to go";
+}
+ else if (a > 50 && a <= 100){
+     return "We'll be there in 5 minutes";
+ }
+ else{
+     return "I'm parking. I'll see you right now";
+ }
+}
+console.log (testNum(total))
